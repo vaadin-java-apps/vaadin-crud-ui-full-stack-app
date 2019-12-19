6140d2bb7fa76d462470de09e7b1398b2300d7db
Lipien <adam@pawtel.pl>
2019-12-19 17:07:19 +0100

    first commit

---
+++ src/main/java/pl/pawtel/cruduiwithvaadin/CrudUiWithVaadinApplication.java
@@ -0,0 +1,13 @@
+package pl.pawtel.cruduiwithvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUiWithVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudUiWithVaadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pawtel/cruduiwithvaadin/CrudUiWithVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawtel.cruduiwithvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUiWithVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
